182a5203c4dc16cfa115dd31418dab35d9f45979
Maksim Shipovskiy <m.shipovskiy@gmail.com>
2021-03-26 16:50:15 +0300

    fixed task-6 (remove/add) instead of replace

--- js/task-6.js
+++ js/task-6.js
@@ -4,8 +4,7 @@ inputField.addEventListener("change", function () {
   if (this.value.length < +this.dataset.length || this.value.length > +this.dataset.length) {
     this.classList.add("invalid")
   } else {
-    this.classList.replace("invalid", "valid")
-    // this.classList.remove("invalid")
-    // this.classList.add("valid")
+    this.classList.remove("invalid")
+    this.classList.add("valid")
   }
 })
